626f4c0e74691e6692d38320966f9558eefc27cb
matymcleod <matymcleod@gmail.com>
2021-07-20 04:05:37 +0000

    adding query_joins

--- 1_queries/1_student_currently_enrolled.sql
+++ 1_queries/1_student_currently_enrolled.sql
@@ -1,4 +1,7 @@
-SELECT name, email, phone
-FROM students
-WHERE github IS NULL
-AND end_date IS NOT NULL;
+-- SELECT name, email, phone
+-- FROM students
+-- WHERE github IS NULL
+-- AND end_date IS NOT NULL;
+
+SELECT students.name as student_name, email, cohorts.name as cohort_name
+FROM students FULL OUTER JOIN cohorts ON cohort_id = cohorts.id;
